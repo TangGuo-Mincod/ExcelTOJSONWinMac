c1ad59ac049906f6ef3cb27abfdf22eba7ff1562
张无忌 <1651801683@qq.com>
2023-06-02 18:08:29 +0800

    添加使用必读

---
+++ 使用必读/JsonData/ScriptFile/TestWE.cs
@@ -0,0 +1,24 @@
+public class TestWE
+{
+    
+    /**
+     * 装备ID UI
+     */    public int id;
+
+    /**
+     * 武器拥有的技能ID UI
+     */    public int[] skillID;
+
+    /**
+     * 武器名称 UI
+     */    public string wepname;
+
+    /**
+     * 武器伤害 UI
+     */    public double wepatt;
+
+    /**
+     * 武器介绍 UI
+     */    public string[] wepmes;
+
+}
